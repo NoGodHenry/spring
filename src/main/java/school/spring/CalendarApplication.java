f8020ec06c8a31ffaa5e0435e2ca545029901cec
NoGodHenry <rts207@seznam.cz>
2026-01-22 10:27:12 +0100

    fix: enumerated gender

--- src/main/java/school/spring/CalendarApplication.java
+++ src/main/java/school/spring/CalendarApplication.java
@@ -2,8 +2,10 @@ package school.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan("school.spring.entity")
 public class CalendarApplication {
     public static void main(String[] args) {
         SpringApplication.run(CalendarApplication.class, args);
